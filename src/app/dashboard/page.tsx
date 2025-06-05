2984db997f1c5f8f48674b985332c798e69af8c7
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2025-06-05 08:13:18 +0100

    setup samie-app boilerplate

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>hello Dashboard</h1>
+    </div>
+  );
+};
+export default Dashboard;
---
+++ src/components/layouts/navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>This is Navbar Components</h1>
+    </div>
+  )
+}
+export default Navbar
