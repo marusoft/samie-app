84317fca8997f61e2b98be222c12a865b0969678
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2025-06-05 09:18:13 +0100

    create a develop branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ export const metadata: Metadata = {
   title: "Samie.",
   description:
     "The simple and secure way to split any bill and settle instantly",
+    icons: {
+      icon: "/favicon.ico",
+      shortcut: "/favicon.ico",
+      apple: "/favicon.ico",
+    }
 };
 
 export default function RootLayout({
